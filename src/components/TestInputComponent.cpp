e1c7de7bfa0bebf01097b886ec79f6b6263677e7
Adrian Cieluch <adrian.cieluch@gmail.com>
2014-09-09 12:18:49 +0200

    Reworked the structure to match cmake compiling

--- src/components/TestInputComponent.cpp
+++ src/components/TestInputComponent.cpp
@@ -10,13 +10,13 @@ void TestInputComponent::update(GameObject& object, sf::Time dt)
 {
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
         sf::Vector2f pos = object.getPosition();
-        pos.x += -1.f;
+        pos.x += -1000.f;
         object.setPosition(pos);
     }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
         sf::Vector2f pos = object.getPosition();
-        pos.x += 1.f;
+        pos.x += 1000.f;
         object.setPosition(pos);
     }
 }
