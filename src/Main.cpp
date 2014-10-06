509b8400064afdd20dbb92a8f22b4b1f889d5c56
Adrian Cieluch <adrian.cieluch@gmail.com>
2014-10-06 14:01:39 +0200

    added image

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 
 int main( int argc, char* argv[] )
 {
-    sf::Vector2i size( 1440, 720 );
+    sf::Vector2i size( 1300, 700 );
     std::string title( "Platformer" );
 
     Game game( size, title );
--- src/gamestates/StartState.cpp
+++ src/gamestates/StartState.cpp
@@ -47,10 +47,16 @@ void StartState::render( Game* game, sf::RenderTarget& target, sf::Time dt )
     sf::Text text;
     text.setString( "Press Enter to start the game" );
     text.setFont( font );
-    text.setPosition( windowSize.x / 2.f - text.getLocalBounds().width/2.f, windowSize.y / 2.f );
+    text.setPosition( windowSize.x / 2.f - text.getLocalBounds().width / 2.f, windowSize.y / 2.f );
     text.setColor( sf::Color::White );
 //    text.setScale( 2.f, 2.f );
     target.draw( text );
+
+    sf::Texture tex;
+    tex.loadFromFile( "media/textures/start.png" );
+    sf::Sprite sprite;
+    sprite.setTexture( tex );
+    target.draw( sprite );
 }
 
 void StartState::processEvents( Game* game, const sf::Event* event )
